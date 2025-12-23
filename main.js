7d2371772bd5fce156f519db391225b2538a958b
waka1mk <51509040+waka1mk@users.noreply.github.com>
2025-12-23 20:05:24 +0900

    Update Supabase URL and ANON_KEY

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // =====================
 // Supabase（自分の値）
 /* ===================== */
-const SUPABASE_URL = "あなたのURL";
-const SUPABASE_ANON_KEY = "あなたのANON_KEY";
+const SUPABASE_URL = "https://ncqfaerpznsopgbpiiso.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5jcWZhZXJwem5zb3BnYnBpaXNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM0NDMwMzMsImV4cCI6MjA3OTAxOTAzM30.K3GOyrE3XVqJtF2fNXYgromkU93es8ag660nHO1Db1g";
 
 const supabaseClient = supabase.createClient(
   SUPABASE_URL,
