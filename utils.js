1eb962a8df66674ab0a97a96776d82860b233343
waka1mk <51509040+waka1mk@users.noreply.github.com>
2025-12-28 23:28:25 +0900

    Refactor Supabase client initialization

--- utils.js
+++ utils.js
@@ -1,8 +1,9 @@
-// utils.js
-const SUPABASE_URL = "https://ncqfaerpznsopgbpiiso.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5jcWZhZXJwem5zb3BnYnBpaXNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM0NDMwMzMsImV4cCI6MjA3OTAxOTAzM30.K3GOyrE3XVqJtF2fNXYgromkU93es8ag660nHO1Db1g";
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-export const supabase = window.supabase.createClient(
+const SUPABASE_URL = "https://ncqfaerpznsopgbpiiso.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5jcWZhZXJwem5zb3BnYnBpaXNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM0NDMwMzMsImV4cCI6MjA3OTAxOTAzM30.K3GOyrE3XVqJtF2fNXYgromkU93es8ag660nHO1Db1g";
+
+export const supabase = createClient(
   SUPABASE_URL,
-  SUPABASE_ANON_KEY
+  SUPABASE_KEY
 );
