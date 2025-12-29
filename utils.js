c52298f3a0a9be3f3e75389634d56fb85c40c0ae
waka1mk <51509040+waka1mk@users.noreply.github.com>
2025-12-29 15:07:28 +0900

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,9 +1,15 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-const SUPABASE_URL = "https://ncqfaerpznsopgbpiiso.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5jcWZhZXJwem5zb3BnYnBpaXNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM0NDMwMzMsImV4cCI6MjA3OTAxOTAzM30.K3GOyrE3XVqJtF2fNXYgromkU93es8ag660nHO1Db1g";
-
 export const supabase = createClient(
-  SUPABASE_URL,
-  SUPABASE_KEY
+  "https://ncqfaerpznsopgbpiiso.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5jcWZhZXJwem5zb3BnYnBpaXNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM0NDMwMzMsImV4cCI6MjA3OTAxOTAzM30.K3GOyrE3XVqJtF2fNXYgromkU93es8ag660nHO1Db1g"
 );
+
+// ピン追加共通関数
+export function addPin(map, post) {
+  const marker = L.marker([post.lat, post.lng]).addTo(map);
+  marker.bindPopup(`
+    <strong>${post.title}</strong><br/>
+    ${post.comment}
+  `);
+}
