3137a966556643f16fd1dd701d14c1fe8ba04c35
waka1mk <51509040+waka1mk@users.noreply.github.com>
2025-12-29 22:36:37 +0900

    Create map.js

---
+++ map.js
@@ -0,0 +1,7 @@
+export const map = L.map("map").setView([35.681236, 139.767125], 13);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "© OpenStreetMap contributors",
+}).addTo(map);
+
+export const markers = [];
