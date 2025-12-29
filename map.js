3d5dc723e17d384f6ee0fc53bbf44b96c492b879
waka1mk <51509040+waka1mk@users.noreply.github.com>
2025-12-29 23:01:40 +0900

    Update map.js

--- map.js
+++ map.js
@@ -1,7 +1,20 @@
+import { supabase } from "./utils.js";
+
 export const map = L.map("map").setView([35.681236, 139.767125], 13);
 
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution: "© OpenStreetMap contributors",
-}).addTo(map);
+L.tileLayer(
+  "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png",
+  { attribution: "© OpenStreetMap" }
+).addTo(map);
 
-export const markers = [];
+// 既存投稿を表示
+const loadPins = async () => {
+  const { data } = await supabase.from("posts").select("*");
+  data.forEach(p => {
+    L.marker([p.lat, p.lng])
+      .addTo(map)
+      .bindPopup(`<b>${p.title}</b><br>${p.comment}`);
+  });
+};
+
+loadPins();
