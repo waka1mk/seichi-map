ce523a696c8b3f0d1f2324cc0b2b032ccd110949
waka1mk <51509040+waka1mk@users.noreply.github.com>
2025-12-30 09:46:21 +0900

    Update map.js

--- map.js
+++ map.js
@@ -1,11 +1,5 @@
-export const map = L.map("map").setView(
-  [35.681236, 139.767125],
-  13
-);
+export const map = L.map("map").setView([35.681236, 139.767125], 13);
 
-L.tileLayer(
-  "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-  {
-    attribution: "&copy; OpenStreetMap contributors"
-  }
-).addTo(map);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "© OpenStreetMap contributors"
+}).addTo(map);
