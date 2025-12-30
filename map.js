bcdce99d98f9cfb145db4b1f6ce51a3487133b68
waka1mk <51509040+waka1mk@users.noreply.github.com>
2025-12-30 09:38:00 +0900

    Update map.js

--- map.js
+++ map.js
@@ -1,7 +1,11 @@
-import L from "https://unpkg.com/leaflet@1.9.4/dist/leaflet-src.esm.js";
-
-export const map = L.map("map").setView([35.681236, 139.767125], 13);
+export const map = L.map("map").setView(
+  [35.681236, 139.767125],
+  13
+);
 
 L.tileLayer(
-  "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+  {
+    attribution: "&copy; OpenStreetMap contributors"
+  }
 ).addTo(map);
