fbf0da1129d3070bffa7e27d04b616f217554218
waka1mk <51509040+waka1mk@users.noreply.github.com>
2025-12-31 19:22:39 +0900

    Update post.js

--- post.js
+++ post.js
@@ -1,29 +1,20 @@
 document.getElementById("postBtn").onclick = () => {
   navigator.geolocation.getCurrentPosition(async (pos) => {
-
-    const userName = localStorage.getItem("user_name") || "anonymous";
-    const content = document.getElementById("content").value.trim();
-
-    if (!content) {
-      alert("内容を入力してください");
-      return;
-    }
-
     await supabase.from("posts").insert([
       {
-        user_name: userName,
-        content: content,
-        lat: Number(pos.coords.latitude),
-        lng: Number(pos.coords.longitude),
-        likes: 0
+        user_name: localStorage.getItem("user_name") || "anonymous",
+        comment: document.getElementById("content").value,
+        lat: pos.coords.latitude,
+        lng: pos.coords.longitude,
+        likes: 0,
       },
     ]);
 
-    const msg = document.getElementById("postComplete");
-    msg.classList.add("show");
+    const toast = document.getElementById("toast");
+    toast.classList.add("show");
 
     setTimeout(() => {
       location.href = "index.html";
-    }, 1600);
+    }, 1500);
   });
 };
