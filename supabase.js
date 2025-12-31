a654255da0a6f209d644eea773b4a26c24c00afa
waka1mk <51509040+waka1mk@users.noreply.github.com>
2025-12-31 10:29:57 +0900

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,6 @@
-// supabase.js
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+export const supabase = createClient(
 const SUPABASE_URL = "https://ncqfaerpznsopgbpiiso.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5jcWZhZXJwem5zb3BnYnBpaXNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM0NDMwMzMsImV4cCI6MjA3OTAxOTAzM30.K3GOyrE3XVqJtF2fNXYgromkU93es8ag660nHO1Db1g";
-window.supabase = supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_KEY
 );
