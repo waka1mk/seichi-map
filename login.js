2c84f9ec1b5e1bd2f2729ca3f0d00f411153ca38
waka1mk <51509040+waka1mk@users.noreply.github.com>
2026-01-01 23:06:38 +0900

    Update login.js

--- login.js
+++ login.js
@@ -1,9 +1,1 @@
-document.getElementById("login").onclick = () => {
-  const name = document.getElementById("name").value;
-  if (!name) {
-    alert("名前を入力してください");
-    return;
-  }
-  localStorage.setItem("user_name", name);
-  location.href = "index.html";
-};
+console.log("login.js loaded");
