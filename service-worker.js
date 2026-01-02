e8a3be73ab1627e77cbefa1c5868a871b83f4f85
waka1mk <51509040+waka1mk@users.noreply.github.com>
2026-01-02 13:17:07 +0900

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('install', e => {
- e.waitUntil(caches.open('seichi-cache').then(c=>c.addAll(['./','index.html','comments.html','comments.js','posts.json'])));
-});
-self.addEventListener('fetch', e => {
- e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-});
