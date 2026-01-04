049dd790380befbfa6a9403b0ef3dc7e347105af
waka1mk <51509040+waka1mk@users.noreply.github.com>
2026-01-04 22:35:08 +0900

    Update post.js

--- post.js
+++ post.js
@@ -1,32 +1,23 @@
-const btn = document.getElementById("postBtn");
+// post.js
+const form = document.getElementById("post-form");
 
-btn.onclick = async () => {
-  const content = document.getElementById("content").value;
+form.onsubmit = async e => {
+  e.preventDefault();
 
-  if (!content) return alert("内容を書いてください");
+  const title = form.title.value;
+  const comment = form.comment.value;
+  const user_name = localStorage.getItem("user_name") ?? "guest";
 
-  const {
-    data: { user }
-  } = await supabase.auth.getUser();
+  navigator.geolocation.getCurrentPosition(async pos => {
+    await supabase.from("posts").insert({
+      title,
+      comment,
+      user_name,
+      lat: pos.coords.latitude,
+      lng: pos.coords.longitude
+    });
 
-  if (!user) {
-    location.href = "login.html";
-    return;
-  }
-
-  const { error } = await supabase.from("posts").insert([
-    {
-      content,
-      user_id: user.id
-    }
-  ]);
-
-  if (error) {
-    console.error(error);
-    alert("投稿失敗");
-    return;
-  }
-
-  document.getElementById("content").value = "";
-  alert("投稿しました");
+    alert("投稿しました！");
+    location.href = "index.html";
+  });
 };
