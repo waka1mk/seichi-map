5e7c9a6bf20681fddd835c45cdc374d849e27a52
waka1mk <51509040+waka1mk@users.noreply.github.com>
2026-01-04 18:27:14 +0900

    Update post.js

--- post.js
+++ post.js
@@ -1,21 +1,32 @@
-document.getElementById("postBtn").addEventListener("click", async () => {
+const btn = document.getElementById("postBtn");
+
+btn.onclick = async () => {
   const content = document.getElementById("content").value;
 
-  if (!content) {
-    alert("内容を書いてください");
+  if (!content) return alert("内容を書いてください");
+
+  const {
+    data: { user }
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    location.href = "login.html";
     return;
   }
 
-  const { error } = await supabase
-    .from("posts")
-    .insert([{ content }]);
+  const { error } = await supabase.from("posts").insert([
+    {
+      content,
+      user_id: user.id
+    }
+  ]);
 
   if (error) {
-    alert("投稿に失敗しました");
     console.error(error);
+    alert("投稿失敗");
     return;
   }
 
   document.getElementById("content").value = "";
   alert("投稿しました");
-});
+};
