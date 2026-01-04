f2825f7bcba76ae33218057f5c4e610e460e4fa0
waka1mk <51509040+waka1mk@users.noreply.github.com>
2026-01-04 23:26:05 +0900

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,20 @@
-console.log("login.js loaded");
+const input = document.getElementById("usernameInput");
+const button = document.getElementById("loginBtn");
+const welcome = document.getElementById("welcomeText");
+
+button.addEventListener("click", () => {
+  const name = input.value.trim();
+
+  if (!name) {
+    alert("名前を入力してください");
+    return;
+  }
+
+  localStorage.setItem("username", name);
+
+  welcome.classList.remove("hidden");
+
+  setTimeout(() => {
+    window.location.href = "index.html";
+  }, 1500);
+});
