f18c8cb06e410d6603f76ad31653b8283280c650
waka1mk <51509040+waka1mk@users.noreply.github.com>
2026-01-04 18:32:24 +0900

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
 const SUPABASE_URL = "https://ncqfaerpznsopgbpiiso.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5jcWZhZXJwem5zb3BnYnBpaXNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM0NDMwMzMsImV4cCI6MjA3OTAxOTAzM30.K3GOyrE3XVqJtF2fNXYgromkU93es8ag660nHO1Db1g";
 
-const supabase = window.supabase.createClient(
+window.supabase = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_ANON_KEY
 );
